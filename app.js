dc78ea362a68046f99c0289e231d6a4600af5240
Jackson <JacksonJD@bv.com>
2020-11-03 21:31:14 -0600

    homework update

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 d3.json("samples.json").then((incomingData) => {
     var data = incomingData.metadata
-    var samples = incomingData.samples
-    
+        
     //Populate dropdown with names
     var dropDown = d3.selectAll("#selDataset")
         data.forEach(d => {
@@ -37,7 +36,48 @@ d3.json("samples.json").then((incomingData) => {
             var tableData = d3.select("tbody").append('tr').append('td')
             tableData.text(`${key}: ${value}`)
         })
+        
+        //Process samples data
+        var samples = incomingData.samples
+        var filteredSample = samples.filter(d => d.id == selectedId)[0]
 
+        // var sortedd = filteredSample.sort(function(a,b) {
+        //     return parseFloat(b.sample_values)-parseFloat(a.sample_values);
+        // });
+
+        var dataSet = []
+        for (var i=0; i<10;i++) {
+            dataSet.push({
+                sampleValue: filteredSample.sample_values[i],
+                sampleTitle: `OTU ${filteredSample.otu_ids[i]}`,
+                sampleLabel: filteredSample.otu_labels[i]
+            })
+        };
+
+        var dataSet = dataSet.reverse();
+
+        var trace1 = {
+            x: dataSet.map(row => row.sampleValue),
+            y: dataSet.map(row => row.sampleTitle),
+            text: dataSet.map(row => row.sampleLabel),
+            name: "Sample Value",
+            type: "bar",
+            orientation: "h"
+        };
+
+        var chartData = [trace1]
+
+        var layout = {
+            title: "Top 10 OTU's",
+            margin: {
+              l: 100,
+              r: 100,
+              t: 100,
+              b: 100
+            }
+        };
+
+        Plotly.newPlot("bar", chartData, layout);
         
     }
 })
