2b1f75fa7c40277255d27a561a5a47f850e5ec29
Jackson <JacksonJD@bv.com>
2020-11-03 20:54:28 -0600

    update

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 d3.json("samples.json").then((incomingData) => {
-    console.log(incomingData)
     var data = incomingData.metadata
-    console.log(data)
-
+    var samples = incomingData.samples
+    
     //Populate dropdown with names
     var dropDown = d3.selectAll("#selDataset")
         data.forEach(d => {
@@ -10,38 +9,35 @@ d3.json("samples.json").then((incomingData) => {
         item.text(d.id)
     });
 
+    //Select Dropdown Control
     var idSelect = d3.select("#selDataset")
-
     idSelect.on("change", runFilter);
 
+    //function to run once dropdown selected value changes
     function runFilter() {
+        //prevent default
         d3.event.preventDefault();
         
+        //Value of dropdown
         var selectedId = idSelect.property("value")
-        // console.log(selectedId);
 
+        //Demographic data 
         var demoData = []
         var demoData = data.filter(d => d.id == selectedId)
-
         var demoList = d3.select("#sample-metadata")
-
         var demoEntries = demoData[0]
-        console.log(demoEntries)
         
+        //remove existing table
         d3.select("table").remove();
+        //append new table
         demoList.append("table").append("tbody")
 
+        //Populate Demographic Info
         Object.entries(demoEntries).forEach(function([key,value]) {
             var tableData = d3.select("tbody").append('tr').append('td')
             tableData.text(`${key}: ${value}`)
-            
-            console.log(key);
-            console.log(value);
         })
+
         
-
     }
-
-    
-
 })
